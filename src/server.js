b7a6fc17d97532962b2a6905c0523e8060eedca0
DavideFranzys <webmaster@franzysonline.it>
2023-08-26 11:20:42 +0200

    add package start

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-server.use(middlewares)
-server.use(router)
-server.listen(8000, () => {
-  console.log('JSON Server is running')
-})
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
 
 
 
