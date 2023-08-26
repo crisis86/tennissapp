16221ad840f62adb8ad5ac3a0513e1cc15b16d03
DavideFranzys <webmaster@franzysonline.it>
2023-08-26 10:44:38 +0200

    json-sever-austostart

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(8000, () => {
+  console.log('JSON Server is running')
+})
+
+
+
+/* const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port); */
