66b6dddbe10d0811b8ec789b078bb5bdc2b1dce7
DavideFranzys <webmaster@franzysonline.it>
2023-08-28 11:42:16 +0200

    changeendpoi33

--- src/Login.js
+++ src/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
             // console.log('proceed');
             //getUsersid();
 
-            fetch("https://tennissapp.vercel.app/user?email=" + email,
+            fetch("http://localhost:5383/user?email=" + email,
             {
                 headers:{
                  accept: 'application/json',
