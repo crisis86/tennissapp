cbbb96990b4d2a4368a0477c3f9e6b376cdfb500
DavideFranzys <webmaster@franzysonline.it>
2023-08-28 09:09:49 +0200

    changeendpoin18

--- src/Login.js
+++ src/Login.js
@@ -65,6 +65,7 @@ const Login = () => {
                     }
                 }
             }).catch((err) => {
+                console.log(err.message)
                 toast.error('Login Failed due to :' + err.message);
             });
         }
