e83bbbf2483caefc391c2dffafc1e5f4d99f5bc5
DavideFranzys <webmaster@franzysonline.it>
2023-08-29 17:35:30 +0200

    vercellnewh

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const jsonServer = require('json-server')
+/* const jsonServer = require('json-server')
 const cors = require('cors')
 const path = require('path')
 
@@ -15,4 +15,34 @@ server.use(router)
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`)
+}) */
+
+const jsonServer = require('json-server')
+const clone = require('clone')
+const data = require('./db.json')
+
+const isProductionEnv = process.env.NODE_ENV === 'production';
+const server = jsonServer.create()
+const PORT = process.env.PORT || 10000; //  chose port from here like 8080, 3001
+
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+    _isFake: isProductionEnv
 })
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+server.use((req, res, next) => {
+    if (req.path !== '/')
+        router.db.setState(clone(data))
+    next()
+})
+
+server.use(router)
+server.listen(PORT, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
