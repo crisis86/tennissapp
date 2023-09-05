60d16421b71f9cee8f3dd8e3c019e21228d0933f
DavideFranzys <webmaster@franzysonline.it>
2023-09-05 09:36:52 +0200

    vercel config5

--- src/api/cron/route.js
+++ src/api/cron/route.js
@@ -4,6 +4,7 @@ export async function GET(request) {
     const sharedKey = searchParams.get('sharedKey');
     if (!sharedKey) {
       return new Response('Failed to get sharedKey', { status: 200 });
-    }
+    } 
+      console.log("eseguo cron alle 10...");
       return new Response('Successfully got sharedKey: '+sharedKey, { status: 200 });
   } 
--- src/api/server.js
+++ src/api/server.js
@@ -1,28 +1,4 @@
-/*  // JSON Server module
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const port = process.env.PORT || 10000; //  chose port from here like 8080, 3001
-
-
-// Make sure to use the default middleware
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
-server.use(router);
-// Listen to port
-server.listen(port, () => {
- console.log("JSON Server is running"+ port);
-});
- */
-
+ // JSON Server module
 
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
