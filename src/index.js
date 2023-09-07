2f834a52395b3eb57c1434dc31f5abd82e1f0cd4
DavideFranzys <webmaster@franzysonline.it>
2023-09-07 10:04:41 +0200

    backend

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import 'react-toastify/dist/ReactToastify.css';
 window.$produrl = "https://tennissapp.onrender.com";
 window.$devurl = "http://localhost:10000";
      
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
