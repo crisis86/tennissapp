f78586ac4ab7386b9cd43ffa40415ab7e9e6925c
DavideFranzys <webmaster@franzysonline.it>
2023-09-07 21:08:36 +0200

    posizione-1

--- src/Challenge-single.js
+++ src/Challenge-single.js
@@ -345,7 +345,7 @@ const ChallengeSingle = () => {
                     console.log("sono io blocca")
                     setsfidabutton(false)
                 }else {
-                    setsfidabutton(true)
+                    setsfidabutton(true )
                     console.log("non sono io")
                 }
             
@@ -437,7 +437,7 @@ const ChallengeSingle = () => {
 
                                                        {sfidabutton && 
                                                     <>
-                                                        {plr.posizione > miaposizione || miaposizione < plr.posizione + 9 &&  // fino a 8 posizione sopra
+                                                        {plr.posizione > miaposizione || miaposizione < plr.posizione + 8 &&  // fino a 8 posizione sopra
 
                                                             <button disabled={plr.insfida === 'true'} onClick={(e) => sfidahandle(e, plr.id, plr.name, 'update')} type="button" className={plr.insfida === 'true' ? 'disabled button button-fill button-small' : 'button button-fill button-small'}>Sfida</button>
 
