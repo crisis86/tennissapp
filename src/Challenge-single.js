c509e791eb938bb1c231a6f643584bf34bac7b20
DavideFranzys <webmaster@franzysonline.it>
2023-09-09 09:30:24 +0200

    regola8

--- src/Challenge-single.js
+++ src/Challenge-single.js
@@ -344,7 +344,7 @@ const ChallengeSingle = () => {
                 if(recorddatalastfida.players[0].idp1 === iduser) {
                     console.log("sono io blocca")
                     setsfidabutton(false)
-                    setgiornisfida(dataconvert.getDate() - dataconvert.getDate());
+                    setgiornisfida(dataconvert.getDate() - datadioggi.getDate());
                 }else {
                     setsfidabutton(true )
                     console.log("non sono io")
