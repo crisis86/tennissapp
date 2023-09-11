5009d7bce65050d101b6d3366919057993acfa71
DavideFranzys <webmaster@franzysonline.it>
2023-09-11 12:43:04 +0200

    modificadata2

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 //import reportWebVitals from './reportWebVitals';
 
-window.$devyl = "https://tennissapp.onrender.com";
-window.$produrl = "http://localhost:10000";
+window.$produrl = "https://tennissapp.onrender.com";
+window.$devurl = "http://localhost:10000";
      
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
