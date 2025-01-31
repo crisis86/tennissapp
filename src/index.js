a939d387ca2e1e72d8829fd1e2de0d8a1c94fa2f
DavideFranzys <webmaster@franzysonline.it>
2025-01-31 14:32:56 +0100

    regole1

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ window.$servEmail = "https://servermail-fhuv.onrender.com";
 
 if (sessionStorage.getItem('club') !== '' || sessionStorage.getItem('club') !== undefined) {
 
-  if (sessionStorage.getItem('club') === 'DM00') {
+  if (sessionStorage.getItem('club') === 'DM00') { //rocca padel
 
     window.$produrl = "https://tennissapp.onrender.com";
   // window.$produrl = "http://localhost:10000";
